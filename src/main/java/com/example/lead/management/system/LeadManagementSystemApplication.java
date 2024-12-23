eff4920711f810043237baaeb635f474a43e3bc2
Rachit Chaurasia <rachit.chaurasia@snackmagic.com>
2024-12-24 01:27:34 +0530

    initial commit

---
+++ src/main/java/com/example/lead/management/system/LeadManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.lead.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadManagementSystemApplication.class, args);
+	}
+
+}
